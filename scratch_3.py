b262d6a9e5b2cae54801b76eabac9da87da47293
catowen <62635681+owen15571@users.noreply.github.com>
2020-05-12 10:38:09 +0800

    人工智障

---
+++ scratch_3.py
@@ -0,0 +1,21 @@
+while True:
+    userinput = input("talk some with me")
+    if userinput == "How are you":
+        print("I am fine ,thank you.")
+
+    if userinput == "baidu":
+        baidu = input("what do you want to search")
+        print("https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&ch=&tn=baiduerr&bar=&wd=" + baidu)
+
+    if userinput == "Hello":
+        print("Hi")
+
+    if userinput == "google":
+        google = input("what do you want to search")
+        print("https://www.google.com/search?q=" + google)
+
+    if userinput == ("help"):
+        print("What's metter with you,You can sand e-mail to owen6666662019@outlook.com")
+
+
+
